c1da2a4f684a04bf5569b48f406f3053120c8c2b
alfredjmg <alfredomujicagodoy@gmail.com>
2020-09-08 16:55:10 -0400

    first commit

---
+++ routes/proyectos.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const router = express.Router();
+const proyectoController = require('../controllers/proyectoController');
+const auth = require('../middleware/auth');
+const { check } = require('express-validator');
+
+// Crear proyectos
+// api/proyectos
+router.post('/',
+    auth,
+    [
+        check('nombre', 'El nombre del proyecto obligatorio').not().isEmpty(),
+    ],
+    proyectoController.crearProyecto
+);
+
+// Obtener todos los proyectos
+router.get('/',
+    auth,
+    proyectoController.obtenerProyectos
+);
+
+// Actualizar proyectos via ID
+router.put('/:id',
+    auth,
+    [
+        check('nombre', 'El nombre del proyecto obligatorio').not().isEmpty(),
+    ],
+    proyectoController.actualizarProyecto
+);
+
+// Eliminar proyectos via ID
+router.delete('/:id',
+    auth,
+    proyectoController.eliminarProyecto
+);
+
+module.exports = router;
